7f2428a4cab482f26f69ecac2a24e1871f6a168f
SebasUNLu <seba.p.marchetti@gmail.com>
2024-10-19 18:22:58 -0300

    Update: base components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer () {
+  return <div>Enterprise 2024</div>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
 import MainTittle from "./components/MainTittle";
+import ProductList from "./components/ProductList";
+import ServiceList from "./components/ServiceList";
+import TeamList from "./components/TeamList";
+import Location from "./components/Location";
+import ContactList from "./components/ContactList";
 
 export default function Home() {
   return (
     <div className="w-full">
       <MainTittle name="Enterprise" />
+      <ProductList />
+      <ServiceList/>
+      <TeamList />
+      <Location />
+      <ContactList />
     </div>
   );
 }
